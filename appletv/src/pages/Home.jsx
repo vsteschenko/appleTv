2d144f9a85d3694483a5a0fcb67f2b683d409cb3
Slava Steshchenko <136891597+vsteschenko@users.noreply.github.com>
2024-04-23 19:37:12 +0200

    Update Home.jsx

--- appletv/src/pages/Home.jsx
+++ appletv/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         <>
             <Nav />
             <Logo />
-            <BackgroundImage src='BackgroundImage.png' alt="A wall full of posters" />
+            <BackgroundImage src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F6PpO1PvfFTV2IHMu38rafti7is6.jpg&f=1&nofb=1&ipt=c077ce216aefb7a3b8c9df5f8b4880f72efbc7cb31c47952446221c683aad6ce&ipo=images' alt="A wall full of posters" />
             <div className="SubInfo">Apple TV+ has it all<br/>Start watching now.</div>
             <button className="ButtonSubscribe">Subscribe</button>
             <ListOfUpNext />
